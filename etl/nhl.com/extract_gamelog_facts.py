9555f1a23061ea7dffb219da3ae6f83b83799b92
krolique <nick@digitalscripter.com>
2018-05-30 18:58:42 -0400

    Updates extract_gamelog_facts.py
    Finilize the module with feature and error checking

--- etl/nhl.com/extract_gamelog_facts.py
+++ etl/nhl.com/extract_gamelog_facts.py
@@ -35,7 +35,7 @@ def fetch_html(team_name, from_year, to_year, game_type,
 
     :param team_name: short name of the team
     :type team_name: str
-    :param from_year: begining year for the season
+    :param from_year: beginning year for the season
     :type from_year: int
     :param to_year: ending year for the season
     :type to_year: int
@@ -50,8 +50,8 @@ def fetch_html(team_name, from_year, to_year, game_type,
     """
 
     # the query string params on the NHL.com side accepts one of two
-    # values 2 or 3 for game tpye. Where 2 means regular and 3 playoffs.
-    # Since we're wrapping this knowledge the rest of the module API
+    # values 2 or 3 for game type. Where 2 means regular and 3 playoffs.
+    # Since we're wrapping this knowledge the rest of the module
     # doesn't need to know about this translation.
     if game_type == 'regular':
         game_type = 2
@@ -61,7 +61,9 @@ def fetch_html(team_name, from_year, to_year, game_type,
         raise RuntimeError(f'game type of {game_type} is not supported')
 
     years = f'{from_year}{to_year}'
-    response = requests.get(url=endpoint_url, timeout=timeout)
+    url = endpoint_url.format(team_name=team_name, years=years,
+                              game_type=game_type)
+    response = requests.get(url=url, timeout=timeout)
     response.raise_for_status()
     return response.text
 
@@ -75,13 +77,9 @@ def extract_title(table_row):
     :rtype: str
     """
 
-    title_txt = table_row.find(name='div').txt
+    title_txt = table_row.find(name='div').text
     title_txt = title_txt.replace('\n', '').replace('\t', '').strip()
-    # every title contains this text, we don't really need the last bit
-    # however if our assumption fails we should find out
-    assert title_txt[20:] == '- Game By Game Stats'
-    title_txt = title_txt[:20]
-
+    title_txt = title_txt.lower()
     return title_txt
 
 
@@ -103,17 +101,29 @@ def extract_col_name(table_row):
     return data
 
 
-def process_html(html_text, game_type):
-    """ """
+def game_facts(html_text, from_year, to_year, game_type):
+    """ Generates a sequence of game facts extracted from an HTML response
+
+    :param html_text: raw HTML text returned from NHL.com site
+    :type html_text: str
+    :param from_year: beginning year for the season
+    :type from_year: int
+    :param to_year: ending year for the season
+    :type to_year: int
+    :param game_type: one of two values regular or playoffs
+    :param game_type: str
+    :returns: a sequence of game facts
+    :rtype: generator
+    """
 
     soup = BeautifulSoup(html_text, 'html.parser')
     table = soup.find(name='table', attrs={'class': 'data'})
-    rows =  table.findAll(name='tr')
+    rows = table.findAll(name='tr')
 
     # The NHL.com static scraping is not an API call. What I've found from
     # observation of current implementation is they will do the following:
     #
-    #   1.  if the team does not have plaoff record they will return regular
+    #   1.  if the team does not have playoff record they will return regular
     #       season game log
     #   2.  if the from year and to year data is not available they will
     #       return latest year game log
@@ -121,12 +131,23 @@ def process_html(html_text, game_type):
     # these facts forces this module to perform a title check to ensure
     # we're not being lied to
     title = extract_title(table_row=rows[0])
-    if title != f'{from_year}-{to_year} {game_type}':
-        raise RuntimeError('game log returned does not meet expectations')
+    expected_prefix = f'{from_year}-{to_year} {game_type}'
 
-    keys = extract_col_name(table_row(row[1]))
+    # the first 17 characters of the title currently follow this format
+    # YYYY-YYYY regular/playoffs. By using this fact we can ensure the
+    # resultant HTML table is the data we expected.
+    if title[:17] != expected_prefix:
+        # an exception would be more noticable if this piece stops working
+        # correcly and an exception would make life harder on the consumer.
+        # i'm not sure which is better yet.
+        return
 
-    return []
+    keys = extract_col_name(table_row=rows[1])
+    for row in rows[2:]:
+        data = [value.text.replace('\n', '').strip()
+                for value in row.findAll(name='td')]
+        yield dict(zip(keys, data))
+
 
 def generate_stream(team_name, from_year, to_year, game_type,
                     output_stream=None):
@@ -134,7 +155,7 @@ def generate_stream(team_name, from_year, to_year, game_type,
 
     :param team_name: short name of the team
     :type team_name: str
-    :param from_year: begining year for the season
+    :param from_year: beginning year for the season
     :type from_year: int
     :param to_year: ending year for the season
     :type to_year: int
@@ -151,7 +172,8 @@ def generate_stream(team_name, from_year, to_year, game_type,
     html = fetch_html(team_name=team_name, from_year=from_year,
                       to_year=to_year, game_type=game_type)
 
-    for message in process_html(html_text=html):
+    for message in game_facts(html_text=html, from_year=from_year,
+                              to_year=to_year, game_type=game_type):
         serialized = dumps(message)
         output_stream.write(f"{serialized}\n")
 
