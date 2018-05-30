890d1aa967999d07ece72714c489d238495c9240
krolique <nick@digitalscripter.com>
2018-05-30 19:03:34 -0400

    Updates dim_helpers.py
    Removed transliterate functionality from this module. This is currently
    available in the util module.

--- etl/nhl.com/dim_helpers.py
+++ etl/nhl.com/dim_helpers.py
@@ -7,8 +7,6 @@
 
 """
 
-from transliterate import translit, detect_language
-
 
 def stream_header(name, fields):
     """ Returns dimensional entity stream header message
@@ -25,21 +23,3 @@ def stream_header(name, fields):
         'name': name,
         'fields': fields
     }
-
-
-def translit_to_latin(value):
-    """ Replaces UTF-8 characters to their latin equivalents
-
-    :param value: the string to transliterate
-    :type value: str
-    :returns: transliterated string
-    :rtype: str
-    """
-
-    # when this library function fails to detect a language
-    # there is nothing to transliterate
-    if not detect_language(value):
-        return value
-    # using the reversed param in the function enables the function to
-    # substitute UTF-8 to latin
-    return translit(value, reversed=True)
